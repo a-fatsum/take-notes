428c2c2f87fedccb226f4d0f749855b0309118bb
moustafa abdelati <moustafaabdelati@moustafas-MacBook-Air.local>
2025-09-13 14:27:31 +1000

    commit

--- script.js
+++ script.js
@@ -23,7 +23,15 @@ function addNote(titleId, elementId) {
   if (!exists) {
     myNotesList.push(note);
   }
+  // Highlight the parent container of the title element
+  const parentContainer = titleElement.parentElement;
+  parentContainer.classList.add("highlight");
 
+  // Update the output area
+  updateOutputArea();
+}
+
+function updateOutputArea() {
   const outputArea = document.getElementById("outputArea");
   outputArea.innerHTML = myNotesList
     .map((n) => `<p>${n.title}</p><p>${n.content}</p>`)
@@ -72,17 +80,102 @@ copyButton.addEventListener("click", copyContents);
 const clearButton = document.getElementById("clearBtn");
 clearButton.addEventListener("click", clearNotes);
 //
+// expectedToChangeButton
 const expectedToChangeButton = document.getElementById(
   "expectedToChangeButton"
 );
-
 expectedToChangeButton.addEventListener("click", (e) => {
   addNote("expectedToChangeTitle", "noteInput2");
-  // console.log("Button clicked:", e.target);
+});
+
+// reasonsForHardshipButton
+const reasonsForHardshipButton = document.getElementById(
+  "reasonsForHardshipButton"
+);
+reasonsForHardshipButton.addEventListener("click", () => {
+  addNote("reasonsForHardshipTitle", "noteInput1");
+});
+
+// How many months button
+const howManyMonthsButton = document.getElementById("howManyMonthsButton");
+howManyMonthsButton.addEventListener("click", () => {
+  addNote("howManyMonthsTitle", "howManyMonthsInput");
+});
+
+// Is the vehicle currently Insured?
+const vehicleCurrentlyInsuredYesCheckbox =
+  document.getElementById("insuredYesCheckbox");
+const vehicleCurrentlyInsuredNoCheckbox =
+  document.getElementById("insuredNoCheckbox");
+//
+vehicleCurrentlyInsuredYesCheckbox.addEventListener("change", () => {
+  toggleCheckbox("isVehicleCurrentlyInsuredTitle", "insuredYesCheckbox");
+  vehicleCurrentlyInsuredNoCheckbox.checked = false;
+});
+vehicleCurrentlyInsuredNoCheckbox.addEventListener("change", () => {
+  // toggleCheckbox("isVehicleCurrentlyInsuredTitle", "insuredNoCheckbox");
+  vehicleCurrentlyInsuredYesCheckbox.checked = false;
+  alert(
+    "Vehicle is not insured. Inform the customer that they are in breach of the conditions of their contract."
+  );
+});
+
+// Is the vehicle currently Registered?
+const vehicleCurrentlyRegisteredYesCheckbox = document.getElementById(
+  "registeredYesCheckbox"
+);
+const vehicleCurrentlyRegisteredNoCheckbox = document.getElementById(
+  "registeredNoCheckbox"
+);
+//
+vehicleCurrentlyRegisteredYesCheckbox.addEventListener("change", () => {
+  toggleCheckbox("isVehicleCurrentlyRegisteredTitle", "registeredYesCheckbox");
+  vehicleCurrentlyRegisteredNoCheckbox.checked = false;
+  // set the registeration state
+  const isRegistered = vehicleCurrentlyRegisteredYesCheckbox.checked;
+  if (isRegistered) {
+    // var myParent = document.body;
+    const parentContainer = document.getElementById(
+      "isVehicleCurrentlyRegisteredTitle"
+    ).parentElement;
+
+    //Create array of options to be added
+    var array = ["Select", "NSW", "QLD", "SA", "TAS", "VIC", "WA", "ACT", "NT"];
+
+    //Create and append select list
+    var selectList = document.createElement("select");
+    // selectList.id = "stateSelect";
+    selectList.classList.add("listOfStates");
+    parentContainer.appendChild(selectList);
+
+    //Create and append the options
+    for (var i = 0; i < array.length; i++) {
+      var option = document.createElement("option");
+      option.value = array[i];
+      option.text = array[i];
+      selectList.appendChild(option);
+    }
+
+    // create a title for the select list
+    var title = document.createElement("h3");
+    title.id = "stateOfRegistrationTitle";
+    title.innerHTML = "State of Registration";
+    parentContainer.insertBefore(title, selectList);
+
+    // add event listener to the select list
+
+    selectList.addEventListener("change", () => {
+      const selectedState = selectList.value;
+      // console.log("Selected state: ", selectedState);
+      addNote("stateOfRegistrationTitle", "stateSelect");
+    });
+  }
 });
 //
-
-// TESTING DYNAMIC BLOCKS ----
-// TESTING DYNAMIC BLOCKS ----
-// TESTING DYNAMIC BLOCKS ----
-// TESTING DYNAMIC BLOCKS ----
+vehicleCurrentlyRegisteredNoCheckbox.addEventListener("change", () => {
+  // toggleCheckbox("isVehicleCurrentlyRegisteredTitle", "registeredNoCheckbox");
+  vehicleCurrentlyRegisteredYesCheckbox.checked = false;
+  alert(
+    "Vehicle is not registered. Inform the customer that they are in breach of the conditions of their contract."
+  );
+});
